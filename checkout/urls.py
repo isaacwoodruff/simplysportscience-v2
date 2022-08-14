42a6a29a5ef1e0e69117d1e4f096069225360318
Isaac Woodruff <isaacjahwoodruff@gmail.com>
2022-08-14 16:10:05 +0100

    Updated csrf code for new Django version

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from checkout.views import checkout_view, webhook_view, success_view, fail_view
-from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', checkout_view, name="checkout"),
     path('payment-success/', success_view, name="success_view"),
     path('payment-failed/', fail_view, name="fail_view"),
-    path('webhook/', csrf_exempt(webhook_view), name="webhook_view"),
+    path('webhook/', webhook_view, name="webhook_view"),
 ]
